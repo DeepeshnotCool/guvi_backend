614d3d313a6e2b278ad7d4052127c53282d2e77e
deepeshnotcool <deepeshkhatri018@gmail.com>
2023-01-01 20:33:33 +0530

    update database

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 mongoose
-  .connect("mongodb://127.0.0.1:27017/guvi-auth", {
+  .connect("mongodb+srv://deepeshranjankhatrimongodb:<password>@cluster0.ekhhqgf.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
