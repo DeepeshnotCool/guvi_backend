9a253d408757a978369c8e375a40bc8fc0a16791
deepeshnotcool <deepeshkhatri018@gmail.com>
2023-01-07 02:12:28 +0530

    update

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 mongoose
-  .connect("mongodb+srv://deepeshranjankhatrimongodb:<password>@cluster0.ekhhqgf.mongodb.net/?retryWrites=true&w=majority", {
+  .connect("mongodb", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
